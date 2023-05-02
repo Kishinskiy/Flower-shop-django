41b2a4129efa9e40eeb20b5f00c02025eb44a8cb
Oleg Kishinskii <oleg.kishinskiy@gmail.com>
2023-05-02 15:02:21 +0300

    add rubrics

--- flower_shop/landing/urls.py
+++ flower_shop/landing/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import home
+from .views import home, by_rubric
 urlpatterns = [
     path('', home, name='home'),
+    path('<int:rubric_id>', by_rubric, name='by_rubric'),
 ]
--- flower_shop/landing/views.py
+++ flower_shop/landing/views.py
@@ -3,12 +3,28 @@ from django.template import loader
 from django.http import HttpResponse
 
 from .models import Bb
+from .models import Rubric
 
 
 def home(request):
-    template = loader.get_template('landing/index.html')
+    # template = loader.get_template('landing/index.html')
     bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
     context = {
-        'bbs': bbs
+        'bbs': bbs,
+        'rubrics': rubrics
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'landing/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {
+        'bbs': bbs,
+        'current_rubric': current_rubric,
+        'rubrics': rubrics
+    }
+
+    return render(request, 'landing/by_rubric.html', context)
