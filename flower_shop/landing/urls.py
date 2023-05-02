663f814e09aa91e14e50356cb90ea084d5079e82
Oleg Kishinskii <oleg.kishinskiy@gmail.com>
2023-05-02 15:29:42 +0300

    add form

--- flower_shop/landing/urls.py
+++ flower_shop/landing/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home, by_rubric
+from .views import home, by_rubric, BbCreateView
 urlpatterns = [
     path('', home, name='home'),
     path('<int:rubric_id>', by_rubric, name='by_rubric'),
+    path('create/', BbCreateView.as_view(), name='create'),
 ]
--- flower_shop/landing/views.py
+++ flower_shop/landing/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 from .models import Bb
 from .models import Rubric
+from .forms import BbForm
 
 
 def home(request):
@@ -28,3 +29,14 @@ def by_rubric(request, rubric_id):
     }
 
     return render(request, 'landing/by_rubric.html', context)
+
+
+class BbCreateView(CreateView):
+    template_name = 'landing/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
